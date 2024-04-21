5d5fb8e40c3963e81e720ce58acd998b1ab9f1a1
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-04-21 11:47:46 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -22,8 +22,7 @@ const Header = () => {
     try {
       await logoutApiCall().unwrap();
       dispatch(logout());
-      // NOTE: here we need to reset cart state for when a user logs out so the next
-      // user doesn't inherit the previous users cart and shipping
+
       dispatch(resetCart());
       navigate('/login');
     } catch (err) {
